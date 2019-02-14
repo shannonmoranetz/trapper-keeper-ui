663a2e6e65aec8eb03b6a3e19df8d0928f5a9659
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-02-14 16:44:11 -0700

    Add test for setError in getNotes

--- src/thunks/index.test.js
+++ src/thunks/index.test.js
@@ -9,7 +9,7 @@ describe("getNotes", () => {
 
   window.fetch = jest.fn().mockImplementation(() =>
     Promise.resolve({
-      json: () => Promise.resolve("stuff"),
+      json: () => Promise.resolve('notes'),
       ok: true
     })
   );
@@ -18,9 +18,28 @@ describe("getNotes", () => {
     mockDispatch = jest.fn();
   });
 
-  it("calls dispatch with setLoading",  () => {
+  it("calls dispatch with setLoading", () => {
     const thunk = getNotes();
     thunk(mockDispatch);
     expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(true));
+  });
+
+  it('should dispatch setLoading(false) and addAllNotes(notes) if the response is ok', async () => {
+    const thunk = getNotes();
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(false));
+    expect(mockDispatch).toHaveBeenCalledWith(actions.addAllNotes('notes'));
+  });
+
+  // ask about undefined status text
+  it('should dispatch setError with an error message if the response is not ok', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      statusText: 'Error fetching, code: undefined',
+      ok: false
+    }))
+    const thunk = getNotes();
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error fetching, code: undefined'));
   });
 });
