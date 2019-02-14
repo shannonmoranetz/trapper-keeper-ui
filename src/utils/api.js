3c59d00d53012a21f4568874d259b2ca281f3d40
Justin <justin@baal-software.com>
2019-02-13 18:58:37 -0700

    Delete comments

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,29 +8,3 @@ export const fetchCall = async (url, option) => {
   }
 };
 
-
-
-// export const getError = response => {
-//   if (!response.ok) throw new Error(`Error fetching, code: ${response.status}`);
-// };
-
-// export const postData = async (path, data) => {
-//   const response = await fetch(`${baseUrl}${path}`, options("POST", data));
-//   getError(response);
-//   const unfilteredData = await response.json();
-//   return unfilteredData.data;
-// };
-
-// export const deleteData = async (path, data) => {
-//   const response = await fetch(`${baseUrl}${path}`, options("DELETE", data));
-//   getError(response);
-//   const unfilteredData = await response.json();
-//   return unfilteredData.results;
-// };
-
-// export const getData = async path => {
-//   const response = await fetch(`${baseUrl}${path}`);
-//   getError(response);
-//   const unfilteredData = await response.json();
-//   return unfilteredData.data;
-// };
