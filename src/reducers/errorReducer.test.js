3344e989a9b8f498910a5ed98cf1df1a1aa0f54e
Justin <justin@baal-software.com>
2019-02-16 21:06:32 -0700

    Complete errorReducer testing

---
+++ src/reducers/errorReducer.test.js
@@ -0,0 +1,19 @@
+import { errorReducer } from "./errorReducer";
+import { setError, setLoading } from "../actions";
+describe("errorReducer", () => {
+  it("should return error message when given correct type", () => {
+    const result = errorReducer(undefined, setError("Did not Post"));
+    expect(result).toEqual("Did not Post");
+  });
+
+  it("should return empty string when wrong action is given and there is no state", () => {
+    const result = errorReducer(undefined, setLoading(null));
+    expect(result).toEqual("");
+  });
+
+  it("should return state when wrong action is given", () => {
+    const result = errorReducer('Did not Post', setLoading(null));
+    expect(result).toEqual("Did not Post");
+  });  
+
+});
--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,11 +1,16 @@
-import { ADD_NEW_NOTE, ADD_ALL_NOTES, UPDATE_NOTE, DELETE_NOTE } from "../actions";
+import {
+  ADD_NEW_NOTE,
+  ADD_ALL_NOTES,
+  UPDATE_NOTE,
+  DELETE_NOTE
+} from "../actions";
 
 export function notesReducer(state = [], action) {
   switch (action.type) {
     case ADD_NEW_NOTE:
       return [...state, action.newNote];
     case ADD_ALL_NOTES:
-      return action.notes
+      return action.notes;
     case UPDATE_NOTE:
       const index = state.findIndex(note => note.id == action.updatedNote.id);
       let tempNotes = state.slice();
