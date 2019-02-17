923357d90593e7619e11b129747e30079ce21c7a
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-02-17 16:14:31 -0700

    Fix snapshot

--- src/containers/ErrorDisplay/ErrorDisplay.test.js
+++ src/containers/ErrorDisplay/ErrorDisplay.test.js
@@ -5,7 +5,7 @@ import { ErrorDisplay } from "./ErrorDisplay";
 describe("ErrorDisplay", () => {
   let wrapper;
   it("should render with no error and match snapshot", () => {
-    wrapper = shallow(<ErrorDisplay />);
+    wrapper = shallow(<ErrorDisplay location='path.com' />);
     expect(wrapper).toMatchSnapshot();
   });
 });
