941269d9619e241730c2b2ccdeb894644fa51378
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-17 14:22:00 -0700

    Add MUI to the header and install Icons package

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { withStyles } from '@material-ui/core/styles'
+import { AppBar, Typography, IconButton, Tooltip } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
 
- const Header = () => (
-  <div className="Header">
-    <h1 className="title">Trapper-Keeper</h1>
-    <Link to="/new-note">Add Note</Link>
-  </div>
+const styles = {
+  toolTip: {
+    maxWidth: 100
+    
+  },
+
+}
+
+const Header = ({ classes }) => (
+  <AppBar>
+      <Typography variant='h1' align='center'>Trapper-Keeper</Typography>
+      <Tooltip title='Add a new note!' placement='bottom-end' className={classes.toolTip}><Link to="/new-note"><IconButton color='secondary'><AddIcon/></IconButton></Link></Tooltip>
+  </AppBar>
 );
 
-export default Header
+export default withStyles(styles)(Header)
