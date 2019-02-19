d33d28c25cf35e43248c1b3a5a08bfbc3c1a7d4b
shannon <shannonrmoranetz@gmail.com>
2019-02-19 16:36:57 -0700

    Add loading display functionality and flex navbar
    Co-authored-by: Dylan <dylan.hofmann@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,8 @@ import AddIcon from '@material-ui/icons/Add'
 const styles = {
   toolTip: {
     maxWidth: 100,
-    backgroundColor: 'primary'
+    height: 100,
+    margin: 25
   },
   title: {
     fontWeight: 900,
@@ -16,22 +17,24 @@ const styles = {
     textShadow: '2px 2px 2px #658c91',
     marginLeft: '10px'
   },
-  iconButton: {
-    margin: 5
+  appBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
-}
+};
 
 export const Header = ({ classes }) => (
-  <AppBar position='sticky' color='primary'>
+  <AppBar position='sticky' color='primary' className={classes.appBar}>
     <Typography variant='display4' className={classes.title}>TrapperKeeper</Typography>
     <Tooltip title='Add a new note!' placement='bottom-end' className={classes.toolTip}>
       <Link to='/new-note'>
-        <IconButton color='secondary' className={classes.iconButton}>
-          <AddIcon />
+        <IconButton color='secondary'>
+          <AddIcon fontSize='large'/>
         </IconButton>
       </Link>
     </Tooltip>
   </AppBar>
 );
 
-export default withTheme()(withStyles(styles)(Header))
+export default withTheme()(withStyles(styles)(Header));
